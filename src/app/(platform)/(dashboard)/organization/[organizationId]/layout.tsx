d36d164e94e9a55f731b61bc01d098cc353db815
Hossein Shayesteh <hosseinxshayesteh@gmail.com>
2024-05-29 16:05:46 +0330

    Updated metadata for improved display on organization pages

--- src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import OrganizationControl from "@/src/app/(platform)/(dashboard)/organization/[organizationId]/_components/OrganizationControl";
+import { auth } from "@clerk/nextjs/server";
+import { startCase } from "lodash";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+  return { title: startCase(orgSlug!) || "organization" };
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
